eac86df3c9c75eda8234ac06b7c165b8946b6fed
JohnChenNA <605161453@qq.com>
2018-05-28 10:39:53 +0800

    ok

--- app/src/main/java/administrator/example/com/fragmentdemo/News/FgNewsListFragment.java
+++ app/src/main/java/administrator/example/com/fragmentdemo/News/FgNewsListFragment.java
@@ -3,15 +3,19 @@ package administrator.example.com.fragmentdemo.News;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
 import java.util.TimerTask;
 
 import administrator.example.com.fragmentdemo.Bean.NewsBean;
+import administrator.example.com.fragmentdemo.ItemNewsAdapter;
 import administrator.example.com.fragmentdemo.News.Presenter.NewsPresenter;
 import administrator.example.com.fragmentdemo.News.View.INewsView;
 import administrator.example.com.fragmentdemo.R;
@@ -19,9 +23,13 @@ import administrator.example.com.fragmentdemo.R;
 public class FgNewsListFragment extends Fragment implements INewsView {
 
     private int type;
-    private TextView tv_news;
+
     private NewsPresenter presenter;
     private SwipeRefreshLayout srl_news;
+    private ItemNewsAdapter adapter;
+    private TextView tv_news_list;
+    private RecyclerView rv_news;
+    private List<NewsBean.Bean> newsBeanList;
 
     public static FgNewsListFragment newInstance(int type){
         Bundle args = new Bundle();
@@ -41,16 +49,22 @@ public class FgNewsListFragment extends Fragment implements INewsView {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         type = getArguments().getInt("type");
-        tv_news = view.findViewById(R.id.tv_news);
+
         srl_news = view.findViewById(R.id.srl_news);
         srl_news.setColorSchemeColors(Color.parseColor("#ffce3d3a"));
         presenter = new NewsPresenter(this);
+
+        adapter = new ItemNewsAdapter(getActivity());
+        rv_news = view.findViewById(R.id.rv_news);
+        tv_news_list = view.findViewById(R.id.tv_news_list);
         srl_news.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                presenter.loadNews(type,0);
+                presenter.loadNews(type , 0);
+
             }
         });
+        presenter.loadNews(type,0);
 //        switch (type){
 //            case FgNewsFragment.NEWS_TYPE_TOP:
 //                tv_news.setText("Top");
@@ -64,26 +78,41 @@ public class FgNewsListFragment extends Fragment implements INewsView {
 //                break;
         }
         public void showNews(final NewsBean newsBean){
-        getActivity().runOnUiThread(new TimerTask() {
-            @Override
-            public void run() {
-                switch (type){
-                    case FgNewsFragment.NEWS_TYPE_TOP:
-                        tv_news.setText(newsBean.getTop().get(0).getTitle()+" "+ newsBean.getTop()
-                        .get(0).getMtime());
-                        break;
-                    case FgNewsFragment.NEW_TYPE_NBA:
-                        tv_news.setText(newsBean.getNba().get(0).getTitle()+" "+ newsBean.getNba()
-                                .get(0).getMtime());
-                        break;
-                    case FgNewsFragment.NEWS_TYPE_JOKES:
-                        tv_news.setText(newsBean.getJoke().get(0).getTitle()+" "+ newsBean.getJoke()
-                                .get(0).getMtime());
-                        break;
-                }
+//        getActivity().runOnUiThread(new TimerTask() {
+//            @Override
+//            public void run() {
+//                switch (type){
+//                    case FgNewsFragment.NEWS_TYPE_TOP:
+//                        news.setText(newsBean.getTop().get(0).getTitle()+" "+ newsBean.getTop()
+//                        .get(0).getMtime());
+//                        break;
+//                    case FgNewsFragment.NEW_TYPE_NBA:
+//                        tv_news.setText(newsBean.getNba().get(0).getTitle()+" "+ newsBean.getNba()
+//                                .get(0).getMtime());
+//                        break;
+//                    case FgNewsFragment.NEWS_TYPE_JOKES:
+//                        tv_news.setText(newsBean.getJoke().get(0).getTitle()+" "+ newsBean.getJoke()
+//                                .get(0).getMtime());
+//                        break;
+//                }
+//            }
+//        });
+            switch (type){
+                case FgNewsFragment.NEWS_TYPE_TOP:
+                    newsBeanList = newsBean.getTop();
+                    break;
+                case FgNewsFragment.NEW_TYPE_NBA:
+                    newsBeanList = newsBean.getNba();
+                    break;
+                case FgNewsFragment.NEWS_TYPE_JOKES:
+                    newsBeanList = newsBean.getJoke();
+                    break;
             }
-        });
-
+            adapter.setData(newsBeanList);
+            rv_news.setLayoutManager(new LinearLayoutManager(getActivity(),
+                    LinearLayoutManager.VERTICAL,false));
+            rv_news.setAdapter(adapter);
+            tv_news_list.setVisibility(View.GONE);
 
     }
     @Override
@@ -98,7 +127,7 @@ public class FgNewsListFragment extends Fragment implements INewsView {
 
     @Override
     public void showErrorMsg(String error) {
-        tv_news.setText("加载失败");
+        tv_news_list.setText("加载失败"+error);
     }
 
 
